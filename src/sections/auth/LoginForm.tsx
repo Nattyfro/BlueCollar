ad0b1404486646ba27c2de12b42d7a6c05f429c0
myles young <myleslewisyoung@gmail.com>
2022-04-03 21:29:52 +0100

    Second Attempt

--- src/sections/auth/LoginForm.tsx
+++ src/sections/auth/LoginForm.tsx
@@ -60,7 +60,7 @@ export default function LoginForm() {
     <form onSubmit={handleSubmit(onSubmit)}>
       <Stack spacing={2.5} alignItems="flex-end">
         <Controller
-          name="emaizl"
+          name="email"
           control={control}
           render={({ field, fieldState: { error } }) => (
             <TextField
