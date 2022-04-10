8d61cfcb16acb25b5b5c2ec52a5c31238f812e3c
myles young <myleslewisyoung@gmail.com>
2022-04-10 11:29:05 +0100

    Eleventh Commit

--- src/sections/@career/contact/CareerContactForm.tsx
+++ src/sections/@career/contact/CareerContactForm.tsx
@@ -204,7 +204,7 @@ export default function CareerContactForm() {
                   sx={{ position: 'absolute', top: 32, right: 16 }}
                 >
                   {icons.map((icon) => (
-                    <Image key={icon} alt="logo card" src={icon} sx={{ height: 24, width: 30 }} />
+                    <Image key={icon} alt="logo card" src={icon} sx={{ height: 48, width: 60 }} />
                   ))}
                 </Stack>
 
