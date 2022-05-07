ac8975566dbc9646c63d9b504fe7ca772620dbb9
myles young <myleslewisyoung@gmail.com>
2022-05-07 20:35:43 +0100

    Third Commit

--- src/sections/@career/jobs/CareerJobCompanyInfo.tsx
+++ src/sections/@career/jobs/CareerJobCompanyInfo.tsx
@@ -12,19 +12,19 @@ type Props = {
 };
 
 export default function CareerJobCompanyInfo({ job }: Props) {
-  const { companyLogo, companyName } = job;
+  const { userAvatar, userName } = job;
 
   return (
     <Paper variant="outlined" sx={{ p: 3, borderRadius: 2, bgcolor: 'background.default' }}>
       <Stack spacing={2} direction="row" alignItems="center">
         <Image
-          alt={companyName}
-          src={companyLogo}
+          alt={userName}
+          src={userAvatar}
           sx={{ width: 48, height: 48, borderRadius: 1 }}
         />
 
         <Stack spacing={0.5}>
-          <Typography variant="h6">{companyName}</Typography>
+          <Typography variant="h6">{userName}</Typography>
           <Link variant="body3" sx={{ color: 'text.secondary' }}>
             View Company Profile
           </Link>
