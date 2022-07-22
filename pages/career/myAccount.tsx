c165f7a93f6da95b19857e26e038b6756caee5f1
myles young <myleslewisyoung@gmail.com>
2022-07-23 00:28:22 +0100

    My Account page first draft section

--- pages/career/myAccount.tsx
+++ pages/career/myAccount.tsx
@@ -1,13 +1,13 @@
 import { ReactElement } from 'react';
 // @mui
 import { styled } from '@mui/material/styles';
-import { Grid, Container, Typography, TextField} from '@mui/material';
+import { Grid, Container, Typography, TextField, Stack, Button} from '@mui/material';
 // config
 import { HEADER_MOBILE_HEIGHT, HEADER_DESKTOP_HEIGHT } from '../../src/config';
 // layouts
 import Layout from '../../src/layouts';
 // components
-import { Page } from '../../src/components';
+import { Iconify, Page } from '../../src/components';
 // sections
 /* import {
   MarketingFreeSEO,
@@ -19,7 +19,9 @@ import {
   
 } from '../../src/sections/@career/contact';
 
-// import {CALENDAR} from '../../public/assets/images/formIcons';
+import { Image } from '../../src/components';
+import securityIcon from '@iconify/icons-carbon/security';
+
 
 // import { CareerDetails, CareerDesiredSalary } from '../../src/sections/@career';
 
@@ -52,16 +54,55 @@ export default function myAccount() {
               <CareerContactInfo />
             </Grid>
             <Grid item xs={12} md={6} lg={6}>
-              <Typography variant="h2" sx={{ mb: 5 }}>
+              {/* <Typography variant="h2" sx={{ mb: 5 }}>
                 Account Settings
-              </Typography>
+              </Typography> */}
+
+            <Stack spacing={6} sx={{mx:1}}>
+
+              <Stack alignItems="center" spacing={1}>
+                <Stack direction="row" alignItems="center" spacing={1}>
+                  <Iconify icon={securityIcon} sx={{ width: 24, height: 24, color: 'primary.main' }} />
+                  <Typography variant="subtitle2">BlueCollar user request</Typography>
+                </Stack> 
+                
+                <Typography variant="caption" sx={{ color: 'text.secondary', textAlign: 'center' }}>
+                Fill out this form and you will be listed on the site within 24hrs
+                </Typography>
+              </Stack>
 
 
-              <Container maxWidth="sm">
-                {/* <CALENDAR/> */}
-                <Typography> jhbfjn </Typography>
-                <TextField name="ijdfn" label="sdsdvf"/>
-              </Container>
+              <Stack alignItems="center" spacing={2} >
+                <Image src="https://blue-collar.vercel.app/assets/images/formIcons/CALENDAR.png" 
+                       alt=""
+                       sx={{width:"50%"}} />
+
+                <Stack direction={{ xs: 'column', md: 'row' }} sx={{width:1}} spacing={1.5}>
+                  <TextField variant="outlined" name="First Name" label="First Name"/>
+                  <TextField variant="outlined" name="Last Name" label="Last Name"/>
+                  <TextField variant="outlined" name="Email" label="Email"/>
+                </Stack>
+              </Stack>
+
+
+              <Stack direction='row' alignItems="center">
+
+                <Button
+                    variant='text' 
+                    size= 'large' 
+                    color="secondary">
+                    Back
+                </Button>
+
+                <Button
+                    variant='contained' 
+                    size= 'large'>
+                  
+                    Next
+                </Button>
+
+              </Stack>
+              </Stack>
 
 
 
